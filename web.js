46772a4271709a77f12127cf9239765f675ff052
Alex Mhungu <alex.mhungu@gmail.com>
2013-07-11 10:45:04 -0700

    Modified web to display data from file on disk

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var DATAFILE_DEFAULT = "index.html";
+
+var fileBuffer = fs.readFileSync(DATAFILE_DEFAULT);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fileBuffer.toString('utf8', 0, fileBuffer.length));
 });
 
 var port = process.env.PORT || 5000;
